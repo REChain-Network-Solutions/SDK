bb70b1d7c7b763a894c48264d0496ebf77e10e52
github-action-benchmark <github@users.noreply.github.com>
2025-09-24 11:43:47 +0000

    add dispute-coordinator-regression-bench (customSmallerIsBetter) benchmark result for e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273

--- bench/dispute-coordinator-regression-bench/data.js
+++ bench/dispute-coordinator-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758707910169,
+  "lastUpdate": 1758714226889,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "dispute-coordinator-regression-bench": [
@@ -9603,6 +9603,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.005337881149999995,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49718502+alexggh@users.noreply.github.com",
+            "name": "Alexandru Gheorghe",
+            "username": "alexggh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273",
+          "message": "[pallet-revive] Add set_storage/set_storage_var_key methods (#9759)\n\n... to be used in polkadot foundry to make sure EVM state is in sync\nwith pallet-revive state.\n\nFixes: https://github.com/paritytech/foundry-polkadot/issues/275\n\n---------\n\nSigned-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>\nCo-authored-by: Alexander Theißen <alex.theissen@me.com>",
+          "timestamp": "2025-09-24T10:33:54Z",
+          "tree_id": "2e58ffa530937bbb0842bb596295fd616ef55271",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273"
+        },
+        "date": 1758714208509,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 23.800000000000004,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 227.09999999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "dispute-coordinator",
+            "value": 0.0026238391399999996,
+            "unit": "seconds"
+          },
+          {
+            "name": "dispute-distribution",
+            "value": 0.008533184749999995,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.00501136068999999,
             "unit": "seconds"
           }
         ]
