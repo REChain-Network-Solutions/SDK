33db7af71e574cf9fa4b0e01d21b5411ea96ea94
github-action-benchmark <github@users.noreply.github.com>
2025-09-24 11:41:59 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,52 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758707801910,
+  "lastUpdate": 1758714118954,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "363911+pepoviola@users.noreply.github.com",
-            "name": "Javier Viola",
-            "username": "pepoviola"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "470bada9507981d7b95384a3e62af7797554cfb6",
-          "message": "bump version of zombienet-sdk (#7964)\n\nBump zombienet-sdk version, including fixes for issues with init\ncontainers.\nThx!",
-          "timestamp": "2025-03-21T10:54:35Z",
-          "tree_id": "047ec9fcec63cdab945716d8e33d78dc77d2f23e",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/470bada9507981d7b95384a3e62af7797554cfb6"
-        },
-        "date": 1742557921052,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Sent to peers",
-            "value": 1.6666666666666665,
-            "unit": "KiB"
-          },
-          {
-            "name": "Received from peers",
-            "value": 307203,
-            "unit": "KiB"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.20191879586666675,
-            "unit": "seconds"
-          },
-          {
-            "name": "availability-recovery",
-            "value": 11.278765376833334,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -21999,6 +21955,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.19672370290000002,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49718502+alexggh@users.noreply.github.com",
+            "name": "Alexandru Gheorghe",
+            "username": "alexggh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273",
+          "message": "[pallet-revive] Add set_storage/set_storage_var_key methods (#9759)\n\n... to be used in polkadot foundry to make sure EVM state is in sync\nwith pallet-revive state.\n\nFixes: https://github.com/paritytech/foundry-polkadot/issues/275\n\n---------\n\nSigned-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>\nCo-authored-by: Alexander Theißen <alex.theissen@me.com>",
+          "timestamp": "2025-09-24T10:33:54Z",
+          "tree_id": "2e58ffa530937bbb0842bb596295fd616ef55271",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/e8f1aff5a174f420cdd77f5d5c854dd6dc8a3273"
+        },
+        "date": 1758714100630,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.20474096289999996,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.195573500300004,
             "unit": "seconds"
           }
         ]
