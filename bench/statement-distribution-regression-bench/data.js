1f03e6f6f637307df58c4c660b3802dcaa2780cf
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 11:48:14 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 8978c005de6631dce20e204380bb43149127cdce

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,52 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758797840713,
+  "lastUpdate": 1758800893836,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "56828990+muraca@users.noreply.github.com",
-            "name": "Matteo Muraca",
-            "username": "muraca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "fd8b1d782cd082a8bf7af98875d33899760d8fb6",
-          "message": "Removed `pallet:getter` from XCM pallets (#7916)\n\n# Description\n\nPart of #3326 \n\nAs per title, the `pallet:getter` usage has been removed from:\n- `pallet-xcm`\n- `xcm-simulator-fuzzer`\n\nGetters have been implemented manually for all storage items where an\nauto-generated one has been removed.\n\npolkadot address: 12poSUQPtcF1HUPQGY3zZu2P8emuW9YnsPduA4XG3oCEfJVp\n\n---------\n\nCo-authored-by: Branislav Kontur <bkontur@gmail.com>",
-          "timestamp": "2025-03-23T21:27:22Z",
-          "tree_id": "1681cf4a8035ffd405d2df6104e82fa0e771220c",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/fd8b1d782cd082a8bf7af98875d33899760d8fb6"
-        },
-        "date": 1742768968864,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Received from peers",
-            "value": 106.39999999999996,
-            "unit": "KiB"
-          },
-          {
-            "name": "Sent to peers",
-            "value": 127.94799999999995,
-            "unit": "KiB"
-          },
-          {
-            "name": "statement-distribution",
-            "value": 0.035938518055999985,
-            "unit": "seconds"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.04595621004799993,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -21999,6 +21955,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04469169570999991,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1728078+michalkucharczyk@users.noreply.github.com",
+            "name": "Michal Kucharczyk",
+            "username": "michalkucharczyk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8978c005de6631dce20e204380bb43149127cdce",
+          "message": "wasmtime: support for perfmap added (#9821)\n\nThis PR add  support for `perfmap` in wasmtime executor.\n\nFor more technical details refer to this\n[doc](https://docs.wasmtime.dev/examples-profiling-perf.html#profiling-with-perfmap).\n\nInstruction on how to configure profiling on substrate nodes (tested\nwith cumulus benchmarks) is\n[here](https://hackmd.io/o_Ghc86OT4KzCE4x04MeOg?view#Getting-the-right-flamegraph).\n\nThe following environment variable needs to be set when executing the\nnode binary:\n```\nexport WASMTIME_PROFILING_STRATEGY=perfmap\n```\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-09-25T10:38:47Z",
+          "tree_id": "5014ce67276b6f7ddaf68d6ea916b50e1937a11d",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/8978c005de6631dce20e204380bb43149127cdce"
+        },
+        "date": 1758800875769,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 127.95599999999995,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.044808458771999966,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.034675648622000003,
             "unit": "seconds"
           }
         ]
